817fa88e36d581ef36edde0df6041105dba7cf62
@albul-k <konstantin.albul@gmail.com>
2020-07-09 00:31:02 +0300

    add diagram + task 4-5

--- lesson_6_task_4.sql
+++ lesson_6_task_4.sql
@@ -1,10 +1,8 @@
 /*
 4.	Подсчитать общее количество лайков десяти самым молодым пользователям (сколько лайков получили 10 самых молодых пользователей).
 */
-SELECT * FROM target_types;
 
-SELECT * FROM likes WHERE target_type_id = 2;
-
+/*
 -- Вывод 10 самых молодых пользователей и их количество лайков
 SELECT
 	(SELECT CONCAT(first_name, ' ', last_name) 
@@ -15,7 +13,29 @@ SELECT
 		WHERE likes.target_id = user_id) AS birthday,
 	COUNT(*) AS likes_amount
 	FROM likes
-	WHERE target_type_id = 2 /*2 - users*/
+	WHERE target_type_id = 2
 	GROUP BY user_name,birthday
 	ORDER BY birthday DESC
 	LIMIT 10;
+*/
+
+-- Вывод 10 самых молодых пользователей и их количество лайков, включая пользователей кто лайки не ставил
+SELECT
+	CONCAT(first_name, ' ', last_name) AS user_name,
+	(SELECT birthday FROM profiles WHERE users.id = user_id) AS birthday,
+	(SELECT COUNT(*) FROM likes WHERE users.id = target_id AND target_type_id = 2) AS likes_amount
+	FROM users
+	ORDER BY birthday DESC
+	LIMIT 10
+
+-- Сумма лайков 10 самых молодых пользователей
+SELECT
+	SUM(likes_amount) AS likes_total
+FROM
+	(SELECT
+		CONCAT(first_name, ' ', last_name) AS user_name,
+		(SELECT birthday FROM profiles WHERE users.id = user_id) AS birthday,
+		(SELECT COUNT(*) FROM likes WHERE users.id = target_id AND target_type_id = 2) AS likes_amount
+		FROM users
+		ORDER BY birthday DESC
+		LIMIT 10) AS total;
--- lesson_6_task_5.sql
+++ lesson_6_task_5.sql
@@ -3,6 +3,7 @@
 (критерии активности необходимо определить самостоятельно).
 */
 
+/*
 -- Вывод 10 пользователей, кто ставит меньше всего лайков
 SELECT
 	(SELECT CONCAT(first_name, ' ', last_name) 
@@ -11,5 +12,14 @@ SELECT
 	COUNT(*) AS likes_amount
 	FROM likes
 	GROUP BY user_name
+	ORDER BY likes_amount
+	LIMIT 10;
+*/
+
+-- Вывод 10 пользователей, кто ставит меньше всего лайков, включая тех, кто лайки не ставил
+SELECT
+	CONCAT(first_name, ' ', last_name) AS user_name,
+	(SELECT COUNT(*) FROM likes WHERE users.id = user_id) AS likes_amount
+	FROM users
 	ORDER BY likes_amount
 	LIMIT 10;
