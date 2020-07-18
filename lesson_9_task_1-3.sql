9a6688d018edbb19f73b986d5d54696a6be9f4b8
@albul-k <konstantin.albul@gmail.com>
2020-07-18 10:51:29 +0300

    done

---
+++ lesson_9_task_1-3.sql
@@ -0,0 +1,34 @@
+/*
+1-3. по желанию) ѕусть имеетс€ таблица с календарным полем created_at. ¬ ней размещены разр€женые календарные записи за август 2018 года '2018-08-01', '2016-08-04', '2018-08-16' и 2018-08-17.
+—оставьте запрос, который выводит полный список дат за август, выставл€€ в соседнем поле значение 1, если дата присутствует в исходном таблице и 0, если она отсутствует.
+*/
+
+USE sample;
+DROP TABLE IF EXISTS task;
+CREATE TABLE task (
+  id SERIAL PRIMARY KEY,
+  created_at DATETIME
+);
+
+INSERT INTO task VALUES
+	(1, '2018-08-01'),
+	(2, NULL),
+	(3, NULL),
+	(4, '2016-08-04'),
+	(5, '2018-08-16'),
+	(6, '2018-08-17');
+
+-- SELECT * from task;
+
+DROP VIEW IF EXISTS my_view;
+CREATE VIEW my_view AS
+SELECT
+	id,
+	created_at,
+	COUNT(created_at)
+FROM
+	task
+GROUP BY
+	id;
+	
+SELECT * FROM my_view;
