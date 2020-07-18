cb9e76c238311f9c8e04d04902a396c4a0484a7e
@albul-k <konstantin.albul@gmail.com>
2020-07-18 10:54:09 +0300

    to utf8

--- lesson_9_task_1-1.sql
+++ lesson_9_task_1-1.sql
@@ -1,15 +1,15 @@
-/*
-1.	В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
+п»ї/*
+1.	Р’ Р±Р°Р·Рµ РґР°РЅРЅС‹С… shop Рё sample РїСЂРёСЃСѓС‚СЃС‚РІСѓСЋС‚ РѕРґРЅРё Рё С‚Рµ Р¶Рµ С‚Р°Р±Р»РёС†С‹, СѓС‡РµР±РЅРѕР№ Р±Р°Р·С‹ РґР°РЅРЅС‹С…. РџРµСЂРµРјРµСЃС‚РёС‚Рµ Р·Р°РїРёСЃСЊ id = 1 РёР· С‚Р°Р±Р»РёС†С‹ shop.users РІ С‚Р°Р±Р»РёС†Сѓ sample.users. РСЃРїРѕР»СЊР·СѓР№С‚Рµ С‚СЂР°РЅР·Р°РєС†РёРё.
 */
 
 DROP TABLE IF EXISTS sample.users;
 CREATE TABLE sample.users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Имя покупателя',
-  birthday_at DATE COMMENT 'Дата рождения',
+  name VARCHAR(255) COMMENT 'РРјСЏ РїРѕРєСѓРїР°С‚РµР»СЏ',
+  birthday_at DATE COMMENT 'Р”Р°С‚Р° СЂРѕР¶РґРµРЅРёСЏ',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'Покупатели';
+) COMMENT = 'РџРѕРєСѓРїР°С‚РµР»Рё';
 
 START TRANSACTION;
 SELECT @id := id, @name := name, @birthday_at := birthday_at FROM shop.users WHERE id = 1;
