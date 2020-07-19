0297c77a055441913e6b28142801320d935eaa53
@albul-k <konstantin.albul@gmail.com>
2020-07-19 23:35:15 +0300

    correction of remarks

--- lesson_9_task_1-3.sql
+++ lesson_9_task_1-3.sql
@@ -1,6 +1,6 @@
 ﻿/*
-1-3. по желанию) ѕусть имеетс¤ таблица с календарным полем created_at. ¬ ней размещены разр¤женые календарные записи за август 2018 года '2018-08-01', '2016-08-04', '2018-08-16' и 2018-08-17.
-—оставьте запрос, который выводит полный список дат за август, выставл¤¤ в соседнем поле значение 1, если дата присутствует в исходном таблице и 0, если она отсутствует.
+1-3. по желанию) Пусть имеется таблица с календарным полем created_at. В ней размещены разряженые календарные записи за август 2018 года '2018-08-01', '2016-08-04', '2018-08-16' и 2018-08-17.
+Составьте запрос, который выводит полный список дат за август, выставляя в соседнем поле значение 1, если дата присутствует в исходном таблице и 0, если она отсутствует.
 */
 
 USE sample;
@@ -16,19 +16,34 @@ INSERT INTO task VALUES
 	(3, NULL),
 	(4, '2016-08-04'),
 	(5, '2018-08-16'),
-	(6, '2018-08-17');
+	(7, '2018-08-17');
 
--- SELECT * from task;
+DROP TABLE IF EXISTS august;
+CREATE TEMPORARY TABLE august (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `day` DATETIME
+);
 
-DROP VIEW IF EXISTS my_view;
-CREATE VIEW my_view AS
+DELIMITER //
+DROP PROCEDURE IF EXISTS august//
+CREATE PROCEDURE august()
+BEGIN
+	DECLARE i INT DEFAULT 31;
+		WHILE i > 0 DO
+			INSERT INTO august VALUES (NULL, CONCAT('2018', '-', '08', '-', i));
+			SET i = i - 1;
+		END WHILE;
+END//
+CALL august()//
+
 SELECT
-	id,
-	created_at,
-	COUNT(created_at)
+	`day`,
+	COUNT(task.created_at) AS counter
 FROM
-	task
+	august
+LEFT JOIN task ON
+	task.created_at = august.day
 GROUP BY
-	id;
-	
-SELECT * FROM my_view;
+	august.id
+ORDER BY
+	august.day//
