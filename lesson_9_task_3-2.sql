6e6983fb97395c378c03cecbc51b24f371cbb7f4
@albul-k <konstantin.albul@gmail.com>
2020-07-19 23:41:06 +0300

    correction of remarks

--- lesson_9_task_3-2.sql
+++ lesson_9_task_3-2.sql
@@ -10,10 +10,11 @@ CREATE TRIGGER check_products_insert BEFORE INSERT ON products
 FOR EACH ROW
 BEGIN
 	IF (COALESCE(NULL, NEW.name, NEW.description) IS NULL) THEN
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'INSERT canceled';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Значение поля name или поля description должно быть отлично от NULL';
 	END IF;
 END//
 
+DELETE FROM products WHERE products.id IN (11, 12, 13)//
 INSERT INTO products VALUES (11, NULL, 'Тестовое описание', 0, 1, NOW(), NOW())//
 INSERT INTO products VALUES (12, 'Тестовое название', NULL, 0, 1, NOW(), NOW())//
 INSERT INTO products VALUES (13, NULL, NULL, 0, 1, NOW(), NOW())//
